94792443dec99dc84f1bd83f3b3fe1fc3abb311e
Elizaveta Matveeva <eliz.matveeva2016@yandex.ru>
2020-04-02 22:18:53 -0500

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,57 +1,41 @@
 import sqlite3
 
 
-def connect():
-    conn = sqlite3.connect("book_store.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, "
-                "isbn INTEGER)")
-    conn.commit()
-    conn.close()
+class Database:
 
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, "
+                         "year INTEGER, isbn INTEGER)")
 
-def view_all():
-    conn = sqlite3.connect("book_store.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM books")
-    books = cur.fetchall()
-    conn.close()
-    return books
+        self.conn.commit()
 
+    def view_all(self):
+        self.cur.execute("SELECT * FROM books")
+        books = self.cur.fetchall()
+        return books
 
-def search(title, author, year, isbn):
-    values = [value if value else "%" for value in (title, author, year, isbn)]  # If some search fields are empty,
-    # they're replaced with a % symbol meaning it can be a string of 0 characters or more
-    conn = sqlite3.connect("book_store.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM books WHERE title LIKE ? AND author LIKE ? AND year LIKE ? AND isbn LIKE ?", values)
-    books = cur.fetchall()
-    conn.close()
-    return books
+    def search(self, title, author, year, isbn):
+        values = [value if value else "%" for value in (title, author, year, isbn)]  # If some search fields are empty,
+        # they're replaced with a % symbol meaning it can be a string of 0 characters or more
+        self.cur.execute("SELECT * FROM books WHERE title LIKE ? AND author LIKE ? AND year LIKE ? AND isbn LIKE ?",
+                         values)
+        books = self.cur.fetchall()
+        return books
 
+    def add(self, title, author, year, isbn):
+        self.cur.execute("INSERT INTO books VALUES(NULL,?,?,?,?)", (title, author, year, isbn))
+        self.conn.commit()
 
-def add(title, author, year, isbn):
-    conn = sqlite3.connect("book_store.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO books VALUES(NULL,?,?,?,?)", (title, author, year, isbn))
-    conn.commit()
-    conn.close()
+    def update(self, id, title, author, year, isbn):
+        self.cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?",
+                         (title, author, year, isbn, id))
+        self.conn.commit()
 
+    def delete(self, id):
+        self.cur.execute("DELETE FROM books WHERE id=?", (id,))
+        self.conn.commit()
 
-def update(id, title, author, year, isbn):
-    conn = sqlite3.connect("book_store.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
-
-
-def delete(id):
-    conn = sqlite3.connect("book_store.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM books WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
-
-
-connect()
+    def __del__(self):
+        self.conn.close()
